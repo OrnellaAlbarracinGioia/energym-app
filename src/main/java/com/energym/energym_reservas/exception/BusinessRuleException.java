10480cf0bb0b06610089aeca168d141372a467cf
OrnellaAlbarracinGioia <ornellalbarracingioia@gmail.com>
2025-12-03 02:07:59 -0300

    Fix Exceptions

---
+++ src/main/java/com/energym/energym_reservas/exception/BusinessRuleException.java
@@ -0,0 +1,7 @@
+package com.energym.energym_reservas.exception;
+
+public class BusinessRuleException extends RuntimeException {
+    public BusinessRuleException(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/energym/energym_reservas/exception/ResourceNotFoundException.java
+++ src/main/java/com/energym/energym_reservas/exception/ResourceNotFoundException.java
@@ -4,4 +4,8 @@ public class ResourceNotFoundException extends RuntimeException {
     public ResourceNotFoundException(String message) {
         super(message);
     }
+
+    public ResourceNotFoundException(String resourceName, String fieldName, Object fieldValue) {
+        super(String.format("%s no encontrado con %s: '%s'", resourceName, fieldName, fieldValue));
+    }
 }
