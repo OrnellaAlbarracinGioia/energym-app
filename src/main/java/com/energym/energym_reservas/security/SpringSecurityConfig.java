8cb27c3029de211667436f939fd12e403bf77bd1
OrnellaAlbarracinGioia <ornellalbarracingioia@gmail.com>
2025-12-09 14:13:53 -0300

    Add dependencies JWT, SpringSecurityConfig and implements UserDetailsService

--- src/main/java/com/energym/energym_reservas/security/SpringSecurityConfig.java
+++ src/main/java/com/energym/energym_reservas/security/SpringSecurityConfig.java
@@ -1,8 +1,12 @@
 package com.energym.energym_reservas.security;
 
+import com.energym.energym_reservas.security.filter.JwtAuthenticationFilter;
+import com.energym.energym_reservas.security.filter.JwtValidationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -18,6 +22,13 @@ import org.springframework.security.web.SecurityFilterChain;
 @RequiredArgsConstructor
 public class SpringSecurityConfig {
 
+    private final AuthenticationConfiguration authenticationConfiguration;
+
+    @Bean
+    AuthenticationManager authenticationManager() throws Exception{
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
     @Bean
     PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
@@ -25,13 +36,16 @@ public class SpringSecurityConfig {
 
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
         return http
                 .csrf(AbstractHttpConfigurer::disable) //Deshabilitamos el token para evitar vulnerabilidades
-                .authorizeHttpRequests(
-                (auth) -> auth
-                .requestMatchers("/api/auth/**").permitAll()
-                .requestMatchers("/v3/api-docs/**", "/swagger-ui/**","/swagger-ui.html","/api-docs/**").permitAll()
-                .anyRequest().authenticated())
+                .authorizeHttpRequests((auth) -> auth
+                    .requestMatchers("/api/auth/**").permitAll()
+                    .requestMatchers("/v3/api-docs/**", "/swagger-ui/**","/swagger-ui.html","/api-docs/**").permitAll()
+                    .anyRequest().authenticated()
+                )
+                .addFilter(new JwtAuthenticationFilter(authenticationManager()))
+                .addFilter(new JwtValidationFilter(authenticationManager()))
                 .sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) //No permite quedar autenticado en la sesión http, es decir que la sesión no tenga estado
                 .build();
     }
--- src/main/java/com/energym/energym_reservas/service/UserService.java
+++ src/main/java/com/energym/energym_reservas/service/UserService.java
@@ -14,6 +14,11 @@ import com.energym.energym_reservas.repository.RoleRepository;
 import com.energym.energym_reservas.repository.SocioRepository;
 import com.energym.energym_reservas.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -24,7 +29,7 @@ import java.util.stream.Collectors;
 @Service
 @RequiredArgsConstructor
 @Transactional
-public class UserService {
+public class UserService implements UserDetailsService {
 
     private final UserRepository userRepository;
     private final RoleRepository roleRepository;
@@ -32,10 +37,19 @@ public class UserService {
     private final SocioRepository socioRepository;
     private final EntrenadorRepository entrenadorRepository;
 
-
     @Transactional(readOnly = true)
-    public List<User> findAll() {
-        return userRepository.findAll();
+    public List<UserResponseDTO> findAll() {
+        List<User> users = userRepository.findAll();
+
+        List<UserResponseDTO> usersDTO = users.stream().map(user -> UserResponseDTO.builder()
+                .id(user.getId())
+                .email(user.getEmail())
+                .roles(user.getRoles().stream().map(Role::getName).collect(Collectors.toSet()))
+                .enabled(user.isEnabled())
+                .build())
+                .toList();
+
+        return usersDTO;
     }
 
     public UserResponseDTO registerUserPublic(UserPublicRequestDTO request) {
@@ -121,4 +135,28 @@ public class UserService {
         entrenadorRepository.save(nuevo);
     }
 
+    @Override
+    @Transactional(readOnly = true)
+    public UserDetails loadUserByUsername(String username){
+        Optional<User> userOpt = userRepository.findByEmail(username);
+
+        if(userOpt.isEmpty()) {
+            throw new UsernameNotFoundException("El Email ingresado no existe");
+        }
+
+        User user =  userOpt.orElseThrow();
+
+        List<GrantedAuthority> authorities = user.getRoles().stream()
+                .map(rol -> new SimpleGrantedAuthority(rol.getName()))
+                .collect(Collectors.toList());
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPassword(),
+                user.isEnabled(),
+                true,
+                true,
+                true,
+                authorities);
+    }
 }
